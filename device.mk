7c77618d58f63d1ece39aa6a25c1c9f0d44f2e31
ShreehariVaasishta <shreehari9481@gmail.com>
2021-05-04 00:24:22 +0530

    juice:overlay:dot: adapt to dot qs notch
    Signed-off-by: aseelps <aseelpsathar@gmail.com>

--- device.mk
+++ device.mk
@@ -193,7 +193,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-dot
 
 # Power
 PRODUCT_PACKAGES += \

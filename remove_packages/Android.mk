f0a415a2bc66ad59f308a5be3f569de7d3fba940
John Paul Patigas <johnpaulpatigas@gmail.com>
2021-07-09 22:01:09 +0800

    juice: Remove prebuilt apps

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup MyVerizonServices OBDM_Permissions OPScreenRecord SprintDM SprintHM YouTube YouTubeMusicPrebuilt VzwOmaTrigger Maps Music RecorderPrebuilt SafetyHubPrebuilt Snap Camera2
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup MyVerizonServices OBDM_Permissions OPScreenRecord SprintDM SprintHM YouTube YouTubeMusicPrebuilt VzwOmaTrigger Maps Music SafetyHubPrebuilt Snap Camera2 AndroidAutoStubPrebuilt arcore CarrierServices Chrome-Stub GalleryGoPrebuilt Ornament SimpleGallery
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null

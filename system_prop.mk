f4f086a7623241028def4475622b2c7a20b14afa
dlwlrma123 <alexfinhart@gmail.com>
2021-07-25 20:37:12 +0800

    Juice: prop: Add dalvik related configs
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -27,6 +27,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
 
+# Dex
+PRODUCT_PROPERTY_OVERRIDES += \
+   pm.dexopt.ab-ota=speed-profile \
+   dalvik.vm.dex2oat-filter=speed \
+   dalvik.vm.image-dex2oat-filter=speed \
+   ro.vendor.qti.am.reschedule_service=true \
+   ro.sys.fw.dex2oat_thread_count=8 \
+   dalvik.vm.boot-dex2oat-threads=8 \
+   dalvik.vm.dex2oat-threads=4 \
+   dalvik.vm.image-dex2oat-threads=4
+   
 # Netflix custom property
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.netflix.bsp_rev=Q6115-31409-1

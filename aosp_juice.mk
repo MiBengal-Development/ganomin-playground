dadcd13f8c06c7afb054f11995d6aacb0e296117
dlwlrma123 <alexfinhart@gmail.com>
2021-07-25 20:21:00 +0800

    Juice: cleanup flags for PE+
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -9,8 +9,10 @@ $(call inherit-product, device/xiaomi/juice/device.mk)
 # Inherit some common PixelExperience stuff.
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+# Adds face unlock if package is available on ROM source.
+TARGET_SUPPORT_FACE_UNLOCK := true
 
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
